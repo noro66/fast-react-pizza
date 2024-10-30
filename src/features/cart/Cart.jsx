0f33db8f51891b162cbf03181a45e7ac03efad11
Nouaamane <norou6@gmail.com>
2024-10-30 17:18:12 +0100

    feat : fixing the loader style

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import Button from "../../ui/Button";
+import LinkButton from "../../ui/LinkButton";
 
 const fakeCart = [
   {
@@ -29,17 +30,14 @@ function Cart() {
 
   return (
     <div>
-      <Link
-        className="text-sm text-blue-500 hover:text-blue-900 hover:underline"
-        to="/menu"
-      >
-        &larr; Back to menu
-      </Link>
+      <LinkButton to={"/menu"}>&larr; Back to menu</LinkButton>
 
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Link to="/order/new">Order pizzas</Link>
+        <Button disabled={false} to="/order/new">
+          Order pizzas
+        </Button>
         <button>Clear cart</button>
       </div>
     </div>
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
-
+import Button from "../../ui/Button";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -48,13 +48,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           <p>{formErrors?.phone}</p>
         </div>
@@ -62,26 +62,29 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
         <div>
           <input
+            className="h-6 w-6 cursor-pointer accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="cursor-pointer" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting} className="bg-yellow-400">
+          <Button disabled={isSubmitting}>
             {isSubmitting ? "Placing order..." : "Order now"}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
@@ -100,12 +103,13 @@ export async function action({ request }) {
     errors.phone =
       "Please give us your correct phone number. We might need it to contact you";
   }
-  if (Object.keys(errors).length > 0) {
-    return errors;
-  }
+  // if (Object.keys(errors).length > 0) {
+  //   return errors;
+  // }
 
-  const newOrder = await createOrder(order);
-  console.log("id :", newOrder.id);
-  return redirect(`/order/${newOrder.id}`);
+  // const newOrder = await createOrder(order);
+  // console.log("id :", newOrder.id);
+  // return redirect(`/order/${newOrder.id}`);
+  return null;
 }
 export default CreateOrder;
